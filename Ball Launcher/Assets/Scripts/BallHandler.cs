4d7e67fe37589dbce5bff1518f8ebf42f7270ecf
Yunti <n.pawcio@gmail.com>
2022-10-22 01:33:59 +0200

    Multiple touch added

--- Ball Launcher/Assets/Scripts/BallHandler.cs
+++ Ball Launcher/Assets/Scripts/BallHandler.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class BallHandler : MonoBehaviour
 {
@@ -25,6 +27,16 @@ public class BallHandler : MonoBehaviour
         SpawnNewBall();
     }
 
+    private void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    private void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -33,7 +45,7 @@ public class BallHandler : MonoBehaviour
             return;
         }
 
-        if(!Touchscreen.current.primaryTouch.press.isPressed)
+        if(Touch.activeTouches.Count == 0)
         {
             if(isDragging)
             {
@@ -47,7 +59,18 @@ public class BallHandler : MonoBehaviour
 
         isDragging = true;
         currentBallRigidbody.isKinematic = true;
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector2 touchPosition = new Vector2();
+
+        foreach(Touch touch in Touch.activeTouches)
+        {
+            touchPosition += touch.screenPosition;
+        }
+
+
+        touchPosition /= Touch.activeTouches.Count;
+
+
         Vector3 worldPosition =  mainCamera.ScreenToWorldPoint(touchPosition);
         currentBallRigidbody.position = worldPosition;
 
